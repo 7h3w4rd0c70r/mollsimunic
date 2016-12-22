ba097590f8f354dba29de65fa5234b763c4173b6
Patrik Šimunič <patriksimunic@hotmail.com>
2016-12-22 21:52:17 +0100

    reactFree

--- src/Router.jsx
+++ src/Router.jsx
@@ -2,6 +2,8 @@
 import React from 'react';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
+
+
 export default (
     <Router history={hashHistory}>
         <Route path='/'>
