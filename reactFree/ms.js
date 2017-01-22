45375081db7e9009d078a5c98c188882997e630c
Patrik Šimunič <patriksimunic@hotmail.com>
2017-01-22 14:33:00 +0100

    ???

--- reactFree/ms.js
+++ reactFree/ms.js
@@ -1,31 +1,90 @@
 
-var
-l = 'en',
-dictionary = {
-    'en': {
-        'header_main': 'Live in your dream',
-        'header_why_us': 'Why us'
-    },
-    'cs': {
-        'header_main': 'Postave si svůj sen',
-        'header_why_us': 'Proč nás'
-    }
-},
-w = function (word) {
-    return dictionary[l][word];
-},
-loadDictionary = function () {
-    $('[data-w]').each(function () {
-        $(this).html(w($(this).data('w')));
-    });
-};
-
 $(document).ready(function () {
-    loadDictionary();
-    $('.lang').click(function () {
-        l = $(this).data('lang');
-        loadDictionary();
-    });
+  
+  $('img.slider1').css('height', '60px');
+  $('img.slider2').css('height', '120px');
+  $('img.slider3').css('height', '60px');
+  
+  $('.slider').on('click', function () {
+      var src = $(this).attr('src');
+      $('#dimmerImage').attr('src', src);
+      $('#dimmer').dimmer('show');
+  });
+  
+  $('.carousel1').on('mouseover', function () {
+    $('img.slider1').animate({
+        height: '120px'
+    }, 750);
+    $('img.slider2').animate({
+        height: '60px'
+    }, 750);
+    $('img.slider3').animate({
+        height: '60px'
+    }, 750);
+  });
+  
+  $('.carousel2').on('mouseover', function () {
+    $('img.slider1').animate({
+        height: '60px'
+    }, 750);
+    $('img.slider2').animate({
+        height: '120px'
+    }, 750);
+    $('img.slider3').animate({
+        height: '60px'
+    }, 750);
+  });
+  
+  $('.carousel3').on('mouseover', function () {
+    $('img.slider1').animate({
+        height: '60px'
+    }, 750);
+    $('img.slider2').animate({
+        height: '60px'
+    }, 750);
+    $('img.slider3').animate({
+        height: '120px'
+    }, 750);
+  });
+  
+  $('.slider1').slick({
+    dots: false,
+    infinite: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    focusOnSelect: true,
+    draggable: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+  
+  $('.slider2').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    focusOnSelect: true,
+    draggable: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+  
+  $('.slider3').slick({
+    dots: false,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    centerMode: true,
+    variableWidth: true,
+    focusOnSelect: true,
+    draggable: true,
+    prevArrow: false,
+    nextArrow: false
+  });
+  
 });
-
 
